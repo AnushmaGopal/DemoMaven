a68535029e69f78a331f5f0db93eb9cc5cc239b5
amalk <amalk@AMAL-NA433>
2025-02-14 16:43:11 +0530

    demo maven first commit

---
+++ demoMavenProject/src/test/java/github/LearnGitHub.java
@@ -0,0 +1,11 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LearnGitHub {
+	@Test
+	public void learn() {
+		Reporter.log("GitHub class Added",true);
+	}
+}
---
+++ demoMavenProject/src/test/java/github/ToLearnPull.java
@@ -0,0 +1,11 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnPull {
+	@Test
+	public void demo() {
+		Reporter.log("ToLearnPull class added",true);
+	}
+}
